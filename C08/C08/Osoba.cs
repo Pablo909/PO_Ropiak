268c3507384c71b6cd4355fdab912275d9ea2b03
Pablo <pawel6bronk@gmail.com>
2019-12-01 21:52:29 +0100

    c08 osoba

---
+++ C08/C08/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C08
+{
+    class Osoba
+    {
+        string imie;
+        int wiek;
+        public Osoba()
+        {
+            imie = "Maciek";
+            wiek = 13;
+        }
+        public Osoba(string imie, int wiek)
+        {
+            this.imie = imie;
+            this.wiek = wiek;
+        }
+        public void ZmienImie(string noweImie)
+        {
+            this.imie = noweImie;
+        }
+
+        public void ZmienWiek(int nowyWiek)
+        {
+            this.wiek = nowyWiek;
+        }
+    }
+}
